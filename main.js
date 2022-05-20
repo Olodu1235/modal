c1f5593beae8f58083cd04f55f26961ed9f90c3e
Deborah <ogochukwuolodu88@gmail.com>
2022-05-20 14:28:31 +1000

    created a modal with javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementById("open-modal").addEventListener
+("click", function(){
+    document.getElementById("overlay").style.display = "block";
+
+})
+document.getElementById("close-modal").addEventListener
+("click", function(){
+    document.getElementById("overlay").style.display = "none";
+
+})
